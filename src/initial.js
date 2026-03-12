053b6f5df91e5346d6f33f4b6b8f91cc8e31f864
magie99 <119002695+magie99@users.noreply.github.com>
2026-03-12 17:16:27 +0000

    Create necessary files and config

---
+++ src/initial.js
@@ -0,0 +1,7 @@
+const content = document.querySelector("#id");
+
+const header = document.createElement("h2");
+header.textContent = "welcome"
+content.appendChild(header);
+
+export default content;
